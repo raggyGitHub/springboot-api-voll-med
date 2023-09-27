2cddee608c6244fc9d7c05b4e92b483382df5c9e
ronald sarmiento <ronisarmont@gmail.com>
2023-09-27 15:05:07 -0400

    version 3 final proyecto spring Alura

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,30 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+
+import med.voll.api.domain.consulta.AgendaDeConsultaService;
+import med.voll.api.domain.consulta.DatosAgendarConsulta;
+import med.voll.api.infra.errores.ValidacionDeIntegridad;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @Autowired
+    private AgendaDeConsultaService service;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos) throws ValidacionDeIntegridad {
+
+        var response = service.agendar(datos);
+
+        return ResponseEntity.ok(response);
+    }
+}
